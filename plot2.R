c26cf5fabcdde0f9a4f00bafad33556c222fcb23
crisfbazz <crisfbazz@gmail.com>
2015-02-08 22:38:03 -0200

    Week1

---
+++ plot2.R
@@ -0,0 +1,15 @@
+## Construct a plot and save it to a PNG file with a width of 480 pixels and a height of 480 pixels.
+
+## read the data from the dates 2007-02-01 and 2007-02-02
+EletricData <- read.table("household_power_consumption.txt", sep = ";", col.names = c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3"), na.strings = "?", skip = 66637, nrows = 2880)
+## paste the column Date with the Time date
+EletricData <- mutate(EletricData, Date_time = paste(Date, Time))
+## convert the Date_time variable to  POSIXct
+EletricData$Date_time <- as.POSIXct(EletricData$Date_time, format = "%d/%m/%Y %H:%M:%S", tz = "UTC")
+
+# plot the Date_time and the Global Active Power with lines
+with(EletricData, plot(Date_time, Global_active_power, type = "l", xlab = "", ylab = "Global Active Power (kilowatts)"))
+
+## copy my plot to a png file
+dev.copy(png, file = "plot2.png", width = 480, height = 480)
+dev.off()
